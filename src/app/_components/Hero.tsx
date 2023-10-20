6d78af7ea041cde5b765f923b02f07f36ee15686
gracebir <grace.devolop@gmail.com>
2023-10-20 18:08:49 +0200

    hero sections

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import data from '@/constant/data.json'
+import { Josefin_Sans } from 'next/font/google'
+
+const josefin = Josefin_Sans({ subsets: ['latin'], weight: ['300'] })
 
 const Hero = () => {
+  const heroInfo = data.hero
   return (
-    <div className='bg-hero-mob w-full lg:bg-hero-desk bg-no-repeat bg-cover h-[70vh]'>
-      
+    <div className={`bg-hero-mob w-full flex items-center lg:bg-hero-desk bg-no-repeat bg-cover h-[80vh]`}>
+      <div className='text-white  wrapper-container'>
+        <div className='w-full lg:w-[55%] border-2 p-4 lg:p-6'>
+          <h1 className={`${josefin.className} text-5xl leading-snug lg:leading-tight lg:text-8xl uppercase`}>
+            {heroInfo.message}
+          </h1>
+        </div>
+      </div>
+
     </div>
   )
 }
