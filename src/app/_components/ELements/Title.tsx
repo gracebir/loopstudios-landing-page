3f0fe6b1ac070902ee8f156f123aea517b628960
gracebir <grace.devolop@gmail.com>
2023-10-21 16:05:16 +0200

    title heading component

--- src/app/_components/ELements/Title.tsx
+++ src/app/_components/ELements/Title.tsx
@@ -1,11 +1,11 @@
 import { Josefin_Sans } from 'next/font/google'
-import React from 'react'
+import React, { FC } from 'react'
 
-const josefin = Josefin_Sans({ subsets: ['latin'], weight: ['300'] })
-const Title = () => {
+const josefin = Josefin_Sans({ subsets: ['latin-ext'], weight: ['300'] })
+const Title: FC<{text: string}> = ({text}) => {
   return (
-    <h1>
-      
+    <h1 className={`${josefin.className} text-center lg:text-start text-3xl lg:text-5xl uppercase font-light`}>
+      {text}
     </h1>
   )
 }
