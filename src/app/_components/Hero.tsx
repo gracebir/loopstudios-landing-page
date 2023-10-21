d62d18dda5f5bf7016a4f755ef02b39674f8fa19
gracebir <grace.devolop@gmail.com>
2023-10-21 15:12:39 +0200

    hero sections text font weight

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
     <div className={`bg-hero-mob w-full flex items-center lg:bg-hero-desk bg-no-repeat bg-cover h-[80vh]`}>
       <div className='text-white  wrapper-container'>
         <div className='w-full lg:max-w-[55%] border-2 p-4 lg:p-6'>
-          <h1 className={`${josefin.className} text-5xl leading-snug lg:leading-tight lg:text-8xl uppercase`}>
+          <h1 className={`${josefin.className} text-5xl leading-snug font-light lg:leading-tight lg:text-8xl uppercase`}>
             {heroInfo.message}
           </h1>
         </div>
