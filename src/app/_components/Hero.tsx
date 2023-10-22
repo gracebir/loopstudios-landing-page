62d64fd199b6916b71f508a09e1b3b5e81706f0b
gracebir <grace.devolop@gmail.com>
2023-10-22 22:25:11 +0200

    hero

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import data from '@/constant/data.json'
 import { Josefin_Sans } from 'next/font/google'
 
-const josefin = Josefin_Sans({ subsets: ['latin'], weight: ['300'] })
+// const josefin = Josefin_Sans({ subsets: ['latin'], weight: ['300'] })
 
 const Hero = () => {
   const heroInfo = data.hero
   return (
     <div className={`bg-hero-mob w-full flex items-center lg:bg-hero-desk bg-no-repeat bg-cover h-[80vh]`}>
       <div className='text-white  wrapper-container'>
-        <div className='w-full lg:max-w-[55%] border-2 p-6 lg:p-10'>
-          <h1 className={`${josefin.className} text-5xl leading-snug font-light lg:leading-tight lg:text-8xl uppercase`}>
+        <div className='w-full lg:max-w-[50%] border-2 p-6 lg:p-8'>
+          <h1 className={`text-5xl leading-snug font-light lg:leading-tight lg:text-8xl uppercase`}>
             {heroInfo.message}
           </h1>
         </div>
