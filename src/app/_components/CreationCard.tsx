86d123195b9ac60596ce47aa732aaf6530b69197
gracebir <grace.devolop@gmail.com>
2023-10-22 11:12:37 +0200

    creation card

--- src/app/_components/CreationCard.tsx
+++ src/app/_components/CreationCard.tsx
@@ -1,14 +1,20 @@
+'use client'
+import { NextFont } from 'next/dist/compiled/@next/font'
 import React, { FC } from 'react'
 
 type creationCardProps = {
     text: string
     bgImage: string
+    font: NextFont
 }
 
-const CreationCard: FC<creationCardProps> = ({ text, bgImage }) => {
+const CreationCard: FC<creationCardProps> = ({ text, bgImage, font }) => {
+    console.log(bgImage)
     return (
-        <div className={`w-full ${bgImage} flex flex-col justify-end bg-cover bg-no-repeat p-6 lg:p-8`}>
-            <h3 className='uppercase text-lg lg:text-xl'>{text}</h3>
+        <div className={`${font.className} w-full ${bgImage} relative lg:h-[58vh] h-[20vh]  bg-cover bg-no-repeat`}>
+            <div className='absolute w-full bg-black bg-opacity-40 top-0 flex flex-col p-6 lg:p-8 justify-end right-0 left-0 bottom-0'>
+                <h3 className='uppercase text-xl lg:text-2xl font-light text-white-color'>{text}</h3>
+            </div>
         </div>
     )
 }
