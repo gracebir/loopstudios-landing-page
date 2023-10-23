507ab798f04282a677c25c94a735509c2883bc5d
gracebir <grace.devolop@gmail.com>
2023-10-23 10:21:35 +0200

    set a z-index

--- src/app/_components/Interactive.tsx
+++ src/app/_components/Interactive.tsx
@@ -8,7 +8,7 @@ const alata = Alata({ subsets: ['latin'], weight: ['400'] })
 const Interactive = () => {
   const interactive = data.overview
   return (
-    <section className="lg:h-[80vh] h-[80vh]lg:block mb-10 flex flex-col gap-8 relative w-full">
+    <section className="lg:h-[80vh] z-10 h-[80vh] lg:block mb-10 flex flex-col gap-8 relative w-full">
       <div className="lg:w-[56%] w-full bg-interactive-mob lg:bg-interactive-desk bg-no-repeat bg-cover h-[30vh] lg:h-[60vh]" />
       <div className="lg:w-[56%] flex items-center w-full bg-white-color lg:absolute  static right-0 top-[20%] h-[50vh]">
         <div className='max-w-xl mx-auto flex flex-col items-center lg:items-start gap-6'>
