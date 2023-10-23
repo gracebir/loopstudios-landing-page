7fc42f38c21b8aa8f88b00c1f599a1bc64913432
gracebir <grace.devolop@gmail.com>
2023-10-23 10:21:03 +0200

    scroll change

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,22 +9,29 @@ import NavModal from './NavModal'
 const Header = () => {
   const navInfo = data.header
   const [isOpen, setIsOpen] = useState(false)
+  const [scrollValue, setScrollValue] = useState(0)
+
+  // scroll behaviar
+  window.addEventListener('scroll', () => {
+    setScrollValue(window.pageYOffset)
+  })
+  console.log(scrollValue)
   return (
-    <header className='fixed left-0 top-0 right-0 py-10'>
+    <header className={`fixed left-0 top-0 z-50 right-0 py-9 lg:py-11 ${scrollValue > 200 ? `bg-black`: `bg-none`}`}>
       <div className='wrapper-container flex justify-between items-center text-white-color'>
         <Link className='z-50' href={'/'}>
           <Image src={navInfo.logo} alt='logo-header' width={200} height={30} />
         </Link>
         <nav className='hidden lg:flex space-x-10'>
-          {navInfo.headerItems.map((item,)=> (
-            <LinkElement key={item.id} href={item.href} text={item.label}/>
+          {navInfo.headerItems.map((item,) => (
+            <LinkElement key={item.id} href={item.href} text={item.label} />
           ))}
         </nav>
         <button onClick={() => setIsOpen(open => !open)} className='block lg:hidden duration-300 z-50'>
-          <Image alt='hamburgeer' src={isOpen ? navInfo.close : navInfo.hamburger} width={30} height={30}/>
+          <Image alt='hamburger' src={isOpen ? navInfo.close : navInfo.hamburger} width={30} height={30} />
         </button>
       </div>
-      {isOpen && (<NavModal/>)}
+      {isOpen && (<NavModal />)}
     </header>
   )
 }
