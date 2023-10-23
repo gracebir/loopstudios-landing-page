e028fb52670d0bc0f48f3a118e9618a3513604e2
gracebir <grace.devolop@gmail.com>
2023-10-23 10:30:45 +0200

    text fixing

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -4,15 +4,14 @@ import data from '@/constant/data.json'
 const Hero = () => {
   const heroInfo = data.hero
   return (
-    <div className={`bg-hero-mob w-full flex items-center lg:bg-hero-desk bg-no-repeat bg-cover h-[80vh]`}>
-      <div className='text-white  wrapper-container'>
-        <div className='w-full lg:max-w-[50%] border-2 p-6 lg:p-8'>
-          <h1 className={`text-5xl leading-snug font-light lg:leading-tight lg:text-8xl uppercase`}>
+    <div className={`bg-hero-mob w-full flex items-center lg:bg-hero-desk bg-no-repeat bg-cover h-[90vh]`}>
+      <div className='text-white wrapper-container flex-1'>
+        <div className='sm:w-full md:w-full lg:max-w-[55%] border-2 px-6 py-3 lg:py-5'>
+          <h1 className={`text-5xl font-light lg:text-[5.5vw]  uppercase`}>
             {heroInfo.message}
           </h1>
         </div>
       </div>
-
     </div>
   )
 }
