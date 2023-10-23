59cb51949fe36624b557d29924ac1b2d085a25b6
gracebir <grace.devolop@gmail.com>
2023-10-23 12:12:57 +0200

    solve footer display

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -10,12 +10,12 @@ const Footer = () => {
   const footerData = data.footer
   return (
     <footer className='bg-black-color py-8 lg:py-14 '>
-      <div className='wrapper-container flex justify-between'>
+      <div className='wrapper-container flex flex-col gap-10 lg:flex-row lg:gap-0 lg:justify-between'>
         <div className='footer-container'>
           <a href="#">
             <Image src={data.header.logo} alt='logo-header' width={200} height={30} />
           </a>
-          <div className='flex flex-col items-center text-white gap-8 lg:flex-row'>
+          <div className='flex flex-col items-center text-white gap-6 lg:gap-8 lg:flex-row'>
             {data.header.headerItems.map((item) => (
              <LinkElement text={item.label} href='#' key={`footer-${item.id}`}/>
             ))}
